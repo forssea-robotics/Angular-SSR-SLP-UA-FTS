4d2865ebead9959e54d2fbab55eb39c0c784eba4
Pierre Sachot <pierre@forssea-robotics.fr>
2023-11-21 10:04:25 +0100

    Add custom webpack config on backend

---
+++ webpack-back.config.js
@@ -0,0 +1,20 @@
+console.log('custom webpack server config');
+
+const webpack = require('webpack');
+
+module.exports = {
+  //target: 'async-node16.11',
+  externals: {
+    externalsPresets: {
+      node: true,
+    },
+    // ADD HERE YOUR CUSTOM EXTERNAL LIBS
+    // lib: 'lib',
+  },
+  experiments: {
+    topLevelAwait: true
+  },
+  plugins:[
+    new webpack.IgnorePlugin({ resourceRegExp: /osx-temperature-sensor$/, }),
+  ]
+};
