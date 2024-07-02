0c44f99d6167414e5dd91d77660cc4932c08fb5f
Andrés MANELLI <amanelli@forssea-robotics.fr>
2024-07-02 14:22:59 +0200

    pass scopes
    Signed-off-by: Andrés MANELLI <amanelli@forssea-robotics.fr>

--- src/backend/manager/slp-manager.service.ts
+++ src/backend/manager/slp-manager.service.ts
@@ -9,15 +9,13 @@ import { Singleton } from 'src/shared/models/abstract-singleton';
 
 export class SlpManagerService extends Singleton<SlpManagerService>() {
 
-  private static readonly _EMPTY_SCOPE = 'DEFAULT';
-
   protected constructor() {
     super();
   }
 
   public getServersTypes(): Promise<string[]>{
     return new Promise((resolve, reject) => {
-      SlpNodejs.findSrvTypes(false).then((result: string[]) => {
+      SlpNodejs.findSrvTypes("NETWORK,LOCAL", false).then((result: string[]) => {
         return resolve(result);
       }).catch((error: any) => {
         console.error(error);
@@ -28,7 +26,7 @@ export class SlpManagerService extends Singleton<SlpManagerService>() {
 
   public getServers(type: string): Promise<Object[]>{
     return new Promise((resolve, reject) => {
-      SlpNodejs.findSrvs(type, SlpManagerService._EMPTY_SCOPE, false).then((result: string[]) => {
+      SlpNodejs.findSrvs(type, "NETWORK", false).then((result: string[]) => {
         return resolve(result);
       }).catch((error: any) => {
         console.error(error);
